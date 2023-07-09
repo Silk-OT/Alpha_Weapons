68c816407261d266e483d0df81d00a3c45ad07ae
Silk-OT <omerntuzun@gmail.com>
2023-07-09 02:49:08 -0400

    Base Creation of Weapons mod using Quilt's Mod Template.

--- src/main/java/com/alphasquad/alpha_weapons/mixin/TitleScreenMixin.java
+++ src/main/java/com/alphasquad/alpha_weapons/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package com.alphasquad.alpha_weapons.mixin;
 
-import com.example.example_mod.ExampleMod;
+import com.alphasquad.alpha_weapons.Alpha_WeaponsMain;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Alpha_WeaponsMain.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
